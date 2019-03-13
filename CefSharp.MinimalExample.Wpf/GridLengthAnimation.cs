3816ef50826b8e7a705103dff3cd28605657a1d8
AP <agaric.perdereau@gmail.com>
2019-03-13 10:36:14 +0100

    add metro and make issue reproductible

---
+++ CefSharp.MinimalExample.Wpf/GridLengthAnimation.cs
@@ -0,0 +1,90 @@
+﻿
+using System;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace CefSharp.MinimalExample.Wpf
+{
+    /// <summary>
+    /// Description of GridLengthAnimation.
+    /// </summary>
+    public class GridLengthAnimation : AnimationTimeline
+    {
+        public override Type TargetPropertyType
+        {
+            get { return typeof(GridLength); }
+        }
+
+        protected override System.Windows.Freezable CreateInstanceCore()
+        {
+            return new GridLengthAnimation();
+        }
+
+        public static readonly DependencyProperty FromProperty =
+            DependencyProperty.Register("From", typeof(GridLength), typeof(GridLengthAnimation));
+
+        public GridLength From
+        {
+            get
+            {
+                return (GridLength)GetValue(GridLengthAnimation.FromProperty);
+            }
+            set
+            {
+                SetValue(GridLengthAnimation.FromProperty, value);
+            }
+        }
+
+        private double FromAsDouble
+        {
+            get
+            {
+                return ((GridLength)From).Value;
+            }
+        }
+
+        public static readonly DependencyProperty ToProperty =
+            DependencyProperty.Register("To", typeof(GridLength), typeof(GridLengthAnimation));
+
+        public GridLength To
+        {
+            get
+            {
+                return (GridLength)GetValue(GridLengthAnimation.ToProperty);
+            }
+            set
+            {
+                SetValue(GridLengthAnimation.ToProperty, value);
+            }
+        }
+
+        private double ToAsDouble
+        {
+            get
+            {
+                return ((GridLength)To).Value;
+            }
+        }
+
+        public GridUnitType GridUnitType
+        {
+            get { return (GridUnitType)GetValue(GridUnitTypeProperty); }
+            set { SetValue(GridUnitTypeProperty, value); }
+        }
+
+        public static readonly DependencyProperty GridUnitTypeProperty =
+            DependencyProperty.Register("GridUnitType", typeof(GridUnitType), typeof(GridLengthAnimation), new UIPropertyMetadata(GridUnitType.Pixel));
+
+        public override object GetCurrentValue(object defaultOriginValue,
+                                               object defaultDestinationValue,
+                                               AnimationClock animationClock)
+        {
+            if (FromAsDouble > ToAsDouble)
+                return new GridLength((1 - animationClock.CurrentProgress.Value) *
+                                      (FromAsDouble - ToAsDouble) + ToAsDouble, this.GridUnitType);
+
+            return new GridLength(animationClock.CurrentProgress.Value *
+                                  (ToAsDouble - FromAsDouble) + FromAsDouble, this.GridUnitType);
+        }
+    }
+}
--- CefSharp.MinimalExample.Wpf/MainWindow.xaml.cs
+++ CefSharp.MinimalExample.Wpf/MainWindow.xaml.cs
@@ -1,12 +1,44 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using MahApps.Metro.Controls;
+using System.Windows.Media.Animation;
 
 namespace CefSharp.MinimalExample.Wpf
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+
+        private void animateColumn(GridLength from, GridLength to, GridUnitType unit, FrameworkContentElement element)
+        {
+            GridLengthAnimation gridLengthAnim = new GridLengthAnimation();
+            gridLengthAnim.GridUnitType = unit;//GridUnitType.Pixel;
+            gridLengthAnim.From = from;
+            gridLengthAnim.To = to;
+            gridLengthAnim.Duration = new Duration(new TimeSpan(0, 0, 0, 0, 250));
+            Storyboard.SetTargetName(gridLengthAnim, element.Name);
+            Storyboard.SetTargetProperty(gridLengthAnim, new PropertyPath("Width"));
+            Storyboard board = new Storyboard();
+            board.Children.Add(gridLengthAnim);
+            board.Begin(this);
+        }
+
+
+        private void toggleBrowserVisibilityButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (browserGridColumn.ActualWidth <1)
+            {
+                animateColumn(new GridLength(0, GridUnitType.Star), new GridLength(2, GridUnitType.Star), GridUnitType.Star, browserGridColumn);
+            }
+            else
+            {
+                animateColumn(new GridLength(2, GridUnitType.Star), new GridLength(0, GridUnitType.Star), GridUnitType.Star, browserGridColumn);
+            }
         }
     }
 }
